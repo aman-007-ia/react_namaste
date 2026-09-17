b82e11e659d37ab17b17e25660a94e845eda49ab
aman-007-ia <amanyadav794172@gmail.com>
2026-09-17 14:52:10 +0530

    part-01

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
-const heading = React.createElement("h1",{},"Hello from CDN")
+const heading = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")
+    ])
+])
+console.log(heading) //object
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(heading)
